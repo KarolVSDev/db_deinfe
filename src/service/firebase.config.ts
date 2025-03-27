9742795d8b9a87d004953b4a3ff2b54e9cb22ccc
0Diogo1 <diogo.ariau@gmail.com>
2025-03-27 08:59:39 -0400

    retirei

--- src/service/firebase.config.ts
+++ src/service/firebase.config.ts
@@ -9,18 +9,6 @@ import env from "../service/env";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-console.log({
-  apiKey: env.VITE_FIREBASE_API_KEY,
-  authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: env.VITE_FIREBASE_APP_ID,
-  measurementId: env.VITE_FIREBASE_MEASUREMENT_ID
-})
-
-
-
 // const firebaseConfig = {
 //   apiKey: env.VITE_FIREBASE_API_KEY,
 //   authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
