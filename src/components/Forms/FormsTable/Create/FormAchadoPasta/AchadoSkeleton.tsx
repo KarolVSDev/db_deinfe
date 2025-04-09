f8c608d9537f6394a8dddc54563680870cdd659f
ViniciusLvr <dev.vncs@gmail.com>
2025-04-09 13:53:48 -0400

    refact/AchadoSkeleton.tsx

--- src/components/Forms/FormsTable/Create/FormAchadoPasta/AchadoSkeleton.tsx
+++ src/components/Forms/FormsTable/Create/FormAchadoPasta/AchadoSkeleton.tsx
@@ -3,22 +3,26 @@ import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
 
 export interface AchadoSkeletonProps {
-  isLoading:boolean;
+  isLoading: boolean;
 }
 
-const AchadoSkeleton:React.FC<AchadoSkeletonProps> = ({isLoading}) => {
+const AchadoSkeleton: React.FC<AchadoSkeletonProps> = ({ isLoading }) => {
   return (
     <Box sx={{ width: '70vw' }}>
-      <Typography variant='h2'>{isLoading && <Skeleton sx={{ml:1, mr:3, width:'25vw', mt:2}} />}</Typography> 
-      <Skeleton sx={{ml:1, mr:3, height:'90px'}} />
-      <Skeleton sx={{ml:1, mr:3, height:'90px'}} />
-      <Skeleton sx={{ml:1, mr:3, height:'75px', width:'10vw'}} />
-      <Skeleton sx={{ml:1, mr:3, height:'75px', width:'15vw'}} />
-      <Skeleton sx={{ml:1, mr:3, height:'120px'}} />
-      <Skeleton sx={{ml:1, mr:3, height:'150px'}} />
-      <Typography variant='h2'>{isLoading && <Skeleton sx={{ml:1, mr:3, width:'25vw', mt:2}} />}</Typography> 
-      <Skeleton sx={{ml:1, mr:3, height:'120px'}} />
-      <Skeleton sx={{ml:1, mr:3, width:'15vw', height:'5vw'}}/> 
+      <Typography variant='h2'>{isLoading && <Skeleton sx={{ ml: 1, mr: 3, width: '25vw', mt: 2 }} />}</Typography>
+      <Skeleton sx={{ ml: 1, mr: 3, height: '90px' }} />
+      <Skeleton sx={{ ml: 1, mr: 3, height: '90px' }} />
+      <Skeleton sx={{ ml: 1, mr: 3, height: '75px', width: '10vw' }} />
+      <Skeleton sx={{ ml: 1, mr: 3, height: '75px', width: '25vw' }} />
+      <Skeleton sx={{ ml: 1, mr: 3, height: '75px', width: '15vw' }} />
+      <Skeleton sx={{ ml: 1, mr: 3, height: '120px' }} />
+      <Skeleton sx={{ ml: 1, mr: 3, height: '150px' }} />
+      <Typography variant='h2'>{isLoading && <Skeleton sx={{ ml: 1, mr: 3, width: '25vw', mt: 2 }} />}</Typography>
+      <Skeleton sx={{ ml: 1, mr: 3, height: '90px' }} />
+      <Skeleton sx={{ ml: 1, mr: 3, height: '75px', width: '10vw' }} />
+      <Typography variant='h2'>{isLoading && <Skeleton sx={{ ml: 1, mr: 3, width: '25vw', mt: 2 }} />}</Typography>
+      <Skeleton sx={{ ml: 1, mr: 3, height: '120px' }} />
+      <Skeleton sx={{ ml: 1, mr: 3, width: '15vw', height: '5vw' }} />
     </Box>
   );
 }
