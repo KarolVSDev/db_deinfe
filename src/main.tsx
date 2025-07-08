461fa37f2803af41ad24c930c02d16d199b7d1d7
ViniciusLvr <dev.vncs@gmail.com>
2025-07-08 15:33:31 -0400

    feat/Criado componente GlobalTheme para responsividade

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from './theme/GlobalTheme.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
-  </React.StrictMode>,
+    </ThemeProvider>
+  </React.StrictMode>
 )
---
+++ src/theme/GlobalTheme.tsx
@@ -0,0 +1,7 @@
+import { createTheme, responsiveFontSizes } from "@mui/material";
+
+const theme = createTheme({
+
+});
+
+export default responsiveFontSizes(theme);
